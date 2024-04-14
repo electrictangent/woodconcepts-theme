de7e3adba6ce3f4f7b9a373a669ef3727dee58e2
electrictangent <electrictangent@protonmail.com>
2024-04-14 18:46:43 +1200

    Added controls to carousel

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -6,6 +6,7 @@ const carouselItems = document.querySelectorAll('.carousel-card');
 let currentIndex = 0;
 let totalItems = carouselItems.length - 2;
 let cardWidth = 33.33333;
+let paused = false;
 
 
 function updateViewport() {
@@ -13,36 +14,50 @@ function updateViewport() {
   viewportWidth = window.innerWidth || document.documentElement.clientWidth;
 
   if(viewportWidth <= 767 ){
-    // Phone viewport
+    // Phone viewport (1 card visible)
     totalItems = carouselItems.length;
     cardWidth = 100;
   } else if(viewportWidth >= 768 && viewportWidth <= 1023){
-    // Tablet viewport
+    // Tablet viewport (2 cards visible)
     totalItems = carouselItems.length - 1;
     cardWidth = 50;
   } else {
-    // Desktop viewport
+    // Desktop viewport (3 card visible)
     totalItems = carouselItems.length - 2;
     cardWidth = 33.333333;
   }
 }
 
-function showNextSlide() {
+function toggleSlide() {
+  // change icon
+  if(!paused){
+    document.getElementById("carousel-pause").className = "fa-solid fa-play";
+  } else {
+    document.getElementById("carousel-pause").className = "fa-solid fa-pause";
+  }
+
+  // toggle slide progression
+  paused = !paused;
+}
+
+function updateCarousel() {
   updateViewport()
+
+  if(!paused){
+    const offset = -currentIndex;
+    carouselInner.style.transform = `translateX(calc(${cardWidth}%  * ${offset} )`;
+  }
+}
+
+function showNextSlide() {
   currentIndex = (currentIndex + 1) % totalItems;
   updateCarousel();
 }
 
 function showPrevSlide() {
-  updateViewport()
   currentIndex = (currentIndex - 1 + totalItems) % totalItems;
   updateCarousel();
 }
 
-function updateCarousel() {
-  updateViewport()
-  const offset = -currentIndex;
-  carouselInner.style.transform = `translateX(calc(${cardWidth}%  * ${offset} )`;
-}
 
 setInterval(showNextSlide, 3000);
